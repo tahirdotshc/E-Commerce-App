b14da9512152cf72b707f510745ec792c47a6a49
tahir <tahir.shc@gmail.com>
2025-08-07 21:58:56 +0500

    jjj

---
+++ my-project/server/src/controllers/product.controller.js
@@ -0,0 +1,97 @@
+import ProductModel from '../models/product.modal.js';
+
+import { v2 as cloudinary } from 'cloudinary';
+import { error } from "console";
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: "dfsumz4wl",
+    api_key: "731292745923114",
+    api_secret: "_jbNc_Zv_cAdBfXi9mHO8l98MRU",
+    secure: true
+});
+
+var imagesArr = [];
+//image upload
+export const uploadImages = async (request, response) => {
+
+
+    try {
+        imagesArr = [];
+
+
+        
+        const image = request.files;
+
+
+
+
+        const options = {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: false
+        };
+        for (let i = 0; i < image?.length; i++) {
+
+            const result = await cloudinary.uploader.upload(image[i].path, options);
+            imagesArr.push(result.secure_url);
+            try {
+                fs.unlinkSync(`uploads/${request.files[i].filename}`);
+            } catch (err) {
+                console.error("File deletion error:", err.message);
+            }
+
+
+
+        }
+
+        return response.status(200).json({
+            images: imagesArr
+        });
+
+    } catch (error) {
+
+
+        return response.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+
+
+
+    }
+}
+
+export async function createProduct(request, response){
+    try {
+        const product = new ProductModel({
+            name: request.body.name,
+            description: request.body.description,
+            images: imagesArr,
+            brand: request.body.brand,
+            price: request.body.price,
+            oldPrice: request.body.oldPrice,
+            catName: request.body.catName,
+            catId: request.body.catId,
+            subCatId: request.body.subCatId,
+            subCat: request.body.subCat,
+            thirdsubCat: request.body.thirdsubCat,
+            thirdsubCatId: request.body.thirdsubCatId,
+            countInStock: request.body.countInStock,
+            rating: request.body.rating,
+            isFeatured: request.body.isFeatured,
+            discount: request.body.discount,
+            
+            
+            
+        });
+    } catch (error) {
+         return response.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+    }
+}
+ 
